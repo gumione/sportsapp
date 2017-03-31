982759a87096dbca363d2db768ec0323a2d2b5a8
gumione <gumione@gmail.com>
2017-03-31 18:34:53 +0300

    structure

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+
+require_once APP . 'config/config.php';
+//core classes
+require_once APP . 'core/model.php';
+require_once APP . 'core/application.php';
+require_once APP . 'core/controller.php';
+require_once APP . 'core/view.php';
+
+$app = new Application();
